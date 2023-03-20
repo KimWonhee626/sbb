0a62a07b214b308d97e1a8e3af1f6c9c21ed552a
KimWonhee626 <kwh7739@gmail.com>
2023-03-20 14:10:50 +0900

    리포지터리 테스트

---
+++ src/main/java/com/mysite/sbb/Question.java
@@ -0,0 +1,30 @@
+package com.mysite.sbb;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter @Setter
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 200)
+    private String subject;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE) // 질문 삭제되면 답변도 같이삭제
+    private List<Answer> answerList;
+}
