f1ced30b83b1ec4d1b95d59e5d1849dce42e4f65
KimWonhee626 <kwh7739@gmail.com>
2023-03-28 16:30:13 +0900

    검색 기능(Specification 인터페이스 사용)

--- src/main/java/com/mysite/sbb/question/QuestionRepository.java
+++ src/main/java/com/mysite/sbb/question/QuestionRepository.java
@@ -4,6 +4,7 @@ package com.mysite.sbb.question;
 import com.mysite.sbb.question.Question;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -13,5 +14,5 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String subject);
     Page<Question> findAll(Pageable pageable); // 페이징
-
+    Page<Question> findAll(Specification<Question> spec, Pageable pageable);
 }
--- src/main/java/com/mysite/sbb/question/QuestionService.java
+++ src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -7,6 +7,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import com.mysite.sbb.answer.Answer;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -42,11 +50,12 @@ public Question getQuestion(Integer id){
         this.questionRepository.save(question);
     }
 
-    public Page<Question> getList(int page) {
+    public Page<Question> getList(int page, String kw) {
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("createDate")); // 작성일시 역순으로 조회(최근 게시물 먼저 보임)
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 게시물 10개씩 페이징
-        return this.questionRepository.findAll(pageable);
+        Specification<Question> spec = search(kw);
+        return this.questionRepository.findAll(spec, pageable);
     }
 
     public void modify(Question question, String subject, String content){
@@ -64,4 +73,36 @@ public Question getQuestion(Integer id){
         question.getVoter().add(siteUser);
         this.questionRepository.save(question);
     }
+
+    // 검색어(kw)를 입력받아 쿼리의 조인문과 where문을 생성하여 리턴
+    /*
+     * q - Root, 즉 기준을 의미하는 Question 엔티티의 객체 (질문 제목과 내용을 검색하기 위해 필요)
+     * u1 - Question 엔티티와 SiteUser 엔티티를 아우터 조인(JoinType.LEFT)하여 만든 SiteUser 엔티티의 객체.
+            Question 엔티티와 SiteUser 엔티티는 author 속성으로 연결되어 있기 때문에
+            q.join("author")와 같이 조인해야 한다. (질문 작성자를 검색하기 위해 필요)
+     * a - Question 엔티티와 Answer 엔티티를 아우터 조인하여 만든 Answer 엔티티의 객체.
+           Question 엔티티와 Answer 엔티티는 answerList 속성으로 연결되어 있기 때문에
+           q.join("answerList")와 같이 조인해야 한다. (답변 내용을 검색하기 위해 필요)
+     * u2 - 바로 위에서 작성한 a 객체와 다시 한번 SiteUser 엔티티와 아우터 조인하여 만든 SiteUser 엔티티의 객체
+           (답변 작성자를 검색하기 위해서 필요)
+     */
+    private Specification<Question> search(String kw){
+        return new Specification<>() {
+            private static final long serialVersionUID = 1L;
+            @Override
+            public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                query.distinct(true);  // 중복을 제거
+                Join<Question, SiteUser> u1 = q.join("author", JoinType.LEFT);
+                Join<Question, Answer> a = q.join("answerList", JoinType.LEFT);
+                Join<Answer, SiteUser> u2 = a.join("author", JoinType.LEFT);
+                return cb.or( // OR 검색
+                        cb.like(q.get("subject"), "%" + kw + "%"), // 제목
+                        cb.like(q.get("content"), "%" + kw + "%"),      // 내용
+                        cb.like(u1.get("username"), "%" + kw + "%"),    // 질문 작성자
+                        cb.like(a.get("content"), "%" + kw + "%"),      // 답변 내용
+                        cb.like(u2.get("username"), "%" + kw + "%"));   // 답변 작성자
+            }
+        };
+    }
+
 }
